3f3fb0ec4a4c63f8adcd1cd4abaf686f9ab84364
Om sharma <omsharma7248@gmail.com>
2025-12-26 23:02:12 +0530

    Update service worker cache version to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'aparna-garden-v4';
+const cacheName = 'aparna-garden-v5';
 const assets = [
   './',
   './index.html',
@@ -57,4 +57,5 @@ self.addEventListener('fetch', e => {
       return cacheRes || fetch(e.request);
     })
   );
+
 });
